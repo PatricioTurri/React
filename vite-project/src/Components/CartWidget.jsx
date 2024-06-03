aa78ddda4c6e642b7d5469779d0a8effacd9d700
PatricioTurri <patricioturri2391@gmail.com>
2024-06-03 04:07:25 -0300

    Preentrega 1 - Patricio Turri

---
+++ vite-project/src/Components/CartWidget.jsx
@@ -0,0 +1,10 @@
+function CartWidget (){
+    return (
+        <>
+        <img src="imagen1.png" alt="img" />
+        <p>0</p>
+        </>
+    )
+}
+
+export default CartWidget
---
+++ vite-project/src/Components/NavBar.jsx
@@ -0,0 +1,16 @@
+import CartWidget from "./CartWidget"
+
+function NavBar () {
+    return (
+        <>
+        <h1>Buenas</h1>
+        <nav>
+            <a href="https://www.instagram.com/soymigrante_/">Instagram</a>
+            <a href="https://www.youtube.com/channel/UCGW3U0Tb5KXtq9BdS-9vuKg">YouTube</a>
+        </nav>
+        <CartWidget/>
+        </>
+    )
+}
+
+export default NavBar
